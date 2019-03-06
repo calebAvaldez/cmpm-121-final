6f329a3ebee1d3f1c31ba7b4e350b85d69616642
Caleb Valdez <caavalde@ucsc.edu>
2019-03-05 21:39:02 -0800

    created player, camera, and collectibles

---
+++ Caleb Valdez - Final/Assets/Scripts/CameraController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    //public Transform pivot;
+
+    private bool menuOpen;
+
+    [SerializeField]
+    private float rotateSpeed = 2.0f, 
+        minViewAngle = -45.0f, 
+        maxViewAngle = 45.0f;
+
+    private float pitch = 0.0f;
+    //private float yaw = 0.0f;
+
+    private void Start()
+    {
+        menuOpen = false;
+        Cursor.visible = false;
+    }
+
+    void LateUpdate()
+    {
+        //if pause menu is on the screen
+        if (menuOpen == false)
+        {
+            //Get x position of mouse and rotate the player
+            float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+            target.Rotate(0, horizontal, 0);
+
+            //Get y position of mouse and rotate the player
+            float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+
+            //Move the camera based on the current rotation of target and original offset
+            float desiredYAngle = target.eulerAngles.y;
+            float desiredXAngle = target.eulerAngles.x;
+            Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+            transform.position = target.position;
+
+            //yaw += rotateSpeed * Input.GetAxis("Mouse X");
+            pitch -= rotateSpeed * Input.GetAxis("Mouse Y");
+            pitch = Mathf.Clamp(pitch, minViewAngle, maxViewAngle);
+
+            transform.rotation = Quaternion.Euler(pitch, target.eulerAngles.y, 0.0f);
+
+            //Limit the up/down camera rotation
+            /*if (transform.rotation.eulerAngles.x > maxViewAngle && transform.rotation.eulerAngles.x < 180.0f)
+            {
+                transform.rotation = Quaternion.Euler(maxViewAngle, target.eulerAngles.y, 0);
+            }
+
+            if (transform.rotation.eulerAngles.x > 180.0f && transform.rotation.eulerAngles.x < minViewAngle)
+            {
+                transform.rotation = Quaternion.Euler(360.0f + minViewAngle, target.eulerAngles.y, 0);
+            }*/
+        }
+    }
+
+    private void Update()
+    {
+       
+    }
+   
+
+}
---
+++ Caleb Valdez - Final/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    public Text countText;
+
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
+    private int count;
+    //private bool menuOpen;
+
+    public AudioClip pickup;
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        controller = GetComponent<CharacterController>();
+        moveSpeed = 7;
+        count = 0;
+        SetCountText();
+    }
+
+    private void FixedUpdate()
+    {
+        //if (menuOpen == false)
+       // {
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0.0f;
+
+            float yStore = moveDirection.y;
+            moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+            moveDirection = moveDirection.normalized * moveSpeed;
+            moveDirection.y = yStore;
+        }
+
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+        controller.Move(moveDirection * Time.deltaTime);
+       // }
+    }
+
+    
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+
+            //audioSource.PlayOneShot(pickup, 0.7f);
+
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = count.ToString();
+    }
+}
